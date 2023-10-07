3b45c3e56722b38908b99b2b1c451332a0e1f3df
mangeshchavan <mangesh.chavan@tejgyan.org>
2023-10-07 19:39:45 +0530

    add description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="AddZoomRegistrant",
+    name="zoom_registrant",
     version="0.1",
+    url='https://github.com/own-coder/add-zoom-registrant',
     description="A package for interacting with Zoom API",
     author="Mangesh Chavan",
     author_email="chavanmangesh245@gmail.com",
     packages=find_packages(),
+    classifiers=[
+        'Topic :: Zoom :: Registrant',
+        'Programming Language :: Python :: 3.7+',
+    ]
 )
