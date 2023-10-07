3d27246f14af8d1ebc374e545055bd46cf47097a
mangeshchavan <mangesh.chavan@tejgyan.org>
2023-10-07 19:03:56 +0530

    first commit with initial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AddZoomRegistrant",
+    version="0.1",
+    description="A package for interacting with Zoom API",
+    author="Mangesh Chavan",
+    author_email="chavanmangesh245@gmail.com",
+    packages=find_packages(),
+)
